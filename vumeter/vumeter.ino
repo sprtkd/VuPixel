8faec5935c3e9ef6d2dffc4a617008ec27dc7a2e
Suprotik Dey <supdey1@gmail.com>
2020-05-11 03:22:51 +0530

    Added pixel support

--- vumeter/vumeter.ino
+++ vumeter/vumeter.ino
@@ -11,19 +11,38 @@
 */
 
 #include <fix_fft.h> // fixed point Fast Fourier Transform library
+#include <FastLED.h>
+#include <LEDMatrix.h>
 #define AUDIO_IN A0 // audio input port
-#define PIXEL_COLS 10 //Not greater than 64
-#define PIXEL_ROWS 20
+#define PIXEL_COLS 20 //Not greater than 64
+#define PIXEL_ROWS 10
+#define MATRIX_SIZE (PIXEL_COLS*PIXEL_ROWS)
+#define MATRIX_LAYOUT VERTICAL_ZIGZAG_MATRIX
 #define FFT_OUTPUT_BUFFER_SIZE 64
 #define READ_BUFFER_SIZE FFT_OUTPUT_BUFFER_SIZE*2
 #define FFT_OUT_MAX_AMP 100
-#define AUDIO_GAIN 5
+#define AUDIO_GAIN 1.2
 #define BAR_GAIN 1
+#define AUDIO_NOISE_THRESHOLD 2
+#define PIXEL_OUTPUT_PIN 12
+#define LED_BRIGHTNESS 70
+#define HUE_SHIFTER 255/PIXEL_COLS
+
+DEFINE_GRADIENT_PALETTE(BlueToRed)
+{
+  0, 0, 0, 255,
+  255,  255, 0, 0
+};
+
+cLEDMatrix < PIXEL_COLS, PIXEL_ROWS, MATRIX_LAYOUT > leds;
+CRGBPalette16 hueBlueToRed = BlueToRed;
 
 void setup() {
-
+  FastLED.addLeds<WS2812B, PIXEL_OUTPUT_PIN, GRB>(leds[0], leds.Size());
+  FastLED.setBrightness(LED_BRIGHTNESS);
+  FastLED.clear(true);
   analogReference(DEFAULT); // use the default analog reference of 5 volts (on 5V Arduino boards)
-  pinMode(A0, INPUT);
+  pinMode(AUDIO_IN, INPUT);
   Serial.begin(9600); // setup serial
 };
 
@@ -34,7 +53,8 @@ void loop() {
   fix_fft(realFFTRes, imaginaryFFTRes, 7, 0); //library call
   convertOutput(realFFTRes, imaginaryFFTRes, fftOutBuf);
   scaleOutput(fftOutBuf, outFreqVals);
-  printData(outFreqVals);
+  //printData(outFreqVals, PIXEL_COLS);
+  lightUpLEDMatrix(outFreqVals);
 };
 
 void readAndScale(char re[], char im[]) {
@@ -44,12 +64,14 @@ void readAndScale(char re[], char im[]) {
   // eg (0 / 4) - 128 = -128 and (1023 / 4) - 128 = 127
   for (byte i = 0; i < READ_BUFFER_SIZE; i++) {
     int sample = analogRead(AUDIO_IN); // read analog input samples from ADC
+    if (sample <= AUDIO_NOISE_THRESHOLD)
+      sample = 0;
     float sampleAmped = (float) sample * AUDIO_GAIN;
     if (sampleAmped > 1023)
       sampleAmped = 1023;
     re[i] = (int) sampleAmped / 4 - 128; // scale the samples to fit within a char variable
     im[i] = 0; // there are no imaginary samples associated with the time domain so set to 0
-  };
+  }
 }
 
 void convertOutput(char re[], char im[], int out[]) {
@@ -86,16 +108,23 @@ void scaleOutput(int fftOut[], int scaledOut[]) {
     scaledOut[i] = (float) scaledOut[i] * BAR_GAIN;
     if (scaledOut[i] > FFT_OUT_MAX_AMP)
       scaledOut[i] = FFT_OUT_MAX_AMP;
-    scaledOut[i] = scaledOut[i] / ((float)FFT_OUT_MAX_AMP / PIXEL_ROWS);
+    scaledOut[i] = map(scaledOut[i], 0, FFT_OUT_MAX_AMP, 0, PIXEL_ROWS);
   }
-
-
 }
 
-void printData(int outData[]) {
-  for (int i = 0; i < PIXEL_COLS; i++) {
+void printData(int outData[], int sizeofData) {
+  for (int i = 0; i < sizeofData; i++) {
     Serial.print(outData[i]); // debug value
     Serial.print(","); // debug value
   }
   Serial.println();
+}
+
+void lightUpLEDMatrix(int ledData[]) {
+  FastLED.clear();
+  for (int i = 0; i < PIXEL_COLS; i++)
+  {
+    leds.DrawLine(i, 0, i, ledData[i], ColorFromPalette(hueBlueToRed, (int)(HUE_SHIFTER * i)));
+  }
+  FastLED.show();
 }
